93d4d89243c21526921f0c042578ab32cd03e0ea
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-02 16:02:28 +0545

    views, template and admin functionality added

--- pollApp/models.py
+++ pollApp/models.py
@@ -6,7 +6,11 @@ from django.utils import timezone
 
 class Question(models.Model):
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateField('date published')
     def __str__(self):
--- pollApp/views.py
+++ pollApp/views.py
@@ -3,14 +3,22 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.urls import reverse
 from django.template import loader
 from .models import Question,Choice
+from django.views import generic
+from django.utils import timezone
+
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('pollApp/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'pollApp/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -30,14 +38,16 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('pollApp:results', args=(question.id,)))
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'pollApp/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'pollApp/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pollApp/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'pollApp/results.html'
